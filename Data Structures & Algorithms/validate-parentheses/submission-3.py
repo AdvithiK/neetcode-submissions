0edcc57cf81f56f9dc21d5eaa54af6eee31340b9
Advithi Kethidi <147006144+AdvithiK@users.noreply.github.com>
2026-08-08 09:57:57 -0700

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        #a stack to take in half the input
+        stack = []
+
+        #dictionary to store matching pairs
+        pairs = {")":"(", "]":"[", "}":"{"}
+
+        #loop through to add the L parenthesis & pop the R parenthesis 
+        for i in s:
+            if i in pairs:
+                if stack and stack[-1] == pairs[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+            
+        
+        #if stack is empty, it was in correct order, else false
+        return not stack
+
+        
