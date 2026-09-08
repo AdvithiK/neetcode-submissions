22756da56d8513041809f222449b2c0d189e070c
Advithi Kethidi <147006144+AdvithiK@users.noreply.github.com>
2026-09-07 19:24:13 -0700

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        #using bitwise XOR for O(1) time
+        n = len(nums)
+        xorr = n
+        #XOR i with nums[i], if same then == 0, else returns i
+        for i in range(n):
+            xorr ^= i ^ nums[i]
+        return xorr
+
+
+        
